86765862cc5f3a47bfaf3188b916ae3b3a649441
CO722414 <maanraman0582@gmail.com>
2019-01-24 16:23:07 -0500

    commmit

--- c0722414/Program.cs
+++ c0722414/Program.cs
@@ -6,10 +6,63 @@ using System.Threading.Tasks;
 
 namespace c0722414
 {
-    class Program
+    public class customer
     {
-        static void Main(string[] args)
+        public int id { get; set; }
+        public String name { get; set; }
+        public void Promote() { }
+
+    var rating= CalculateRating();
+    }
+    public int CalculateRating();
+    if (rating==0)
         {
+        Console.WriteLine("Promoted to Level 1");
+
+}
+else{Console.WriteLine("Promoted to Level 2);}
+    }
+    public int CalculateRating()
+{
+    return 0;
+}
+class Program
+   {
+    static void Main(String[] args)
+        {
+            var customer = new customer();
+            customer.
+            //var dog_and_cat = new car();
+            // var bowling = new car();
+            //// Console.WriteLine("how many cars:{0}", Vehicle.howmanycars);
+            //book mybook = new book();
+            //mybook.SetTitle("A Separate Peace");
+            ////Console.WriteLine("my color is" + color);
         }
     }
-}
+     //class Vehicle
+     //   class car
+        class bookcollection
+    {
+
+    }
+    class book : bookcollection
+    {
+        private String Title;
+        public void SetTitle(String aTitle) { this.Title = aTitle; }
+        public String GetTitle() { return this.Title; }       //public static int howmanycars =0;
+       // public String color = "Blue";
+    }
+     //class car : Vehicle
+     //   {
+     //   public String color = "red";
+     //   public car()
+     //   {
+     //       Vehicle.howmanycars++;
+        
+     //   Console.WriteLine("my color is " + color);
+        }
+        
+        
+    
+
